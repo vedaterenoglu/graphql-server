a2616d7feedfc7216f7667d3f5895ec70fddc131
VEDAT ERENOGLU <vedaterenoglu@imacve.local>
2018-08-30 11:49:02 +0200

    ES6

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ index.js by Vedat Erenoglu
 
 //we import the ApolloServer and gql from 'apollo-server'
 
-  const { ApolloServer, gql } = require('apollo-server');
+import { ApolloServer, gql } from 'apollo-server';
 
   // This is a (sample) collection of books we'll be able to query the GraphQL server for.  A more complete example might fetch from an existing data source like a REST API or database.
 
